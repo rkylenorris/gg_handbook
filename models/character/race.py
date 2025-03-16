eaff1e85ee0f96f932ac0608c8ea4d1fd2183215
Kyle Norris <r.kyle.norris@gmail.com>
2025-03-16 11:14:09 -0400

    added race classes/models

--- models/character/race.py
+++ models/character/race.py
@@ -17,4 +17,21 @@ class RacialTrait(Base):
     race_id = Column(Integer, ForeignKey('races.id'))
     name = Column(String)
     description = Column(String)
+
+class SubRace(Base):
+    __tablename__ = 'subraces'
     
+    id = Column(Integer, primary_key=True, index=True)
+    race_id = Column(Integer, ForeignKey('races.id'))
+    name = Column(String)
+    description = Column(String)
+    traits = relationship("RacialTrait", back_populates='SubRace')
+    
+class RaceSkillBonus(Base):
+    __tablename__ = 'race_skill_bonuses'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    race_id = Column(Integer, ForeignKey('races.id'))
+    skill_id = Column(Integer, ForeignKey('skills.id'))
+    bonus = Column(Integer) # bonus to the skill
+    description = Column(String) # description of why the bonus is given
