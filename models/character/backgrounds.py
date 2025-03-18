ef0e9716c9a62b4a5ac34789b2937dfdb1f07e80
Kyle Norris <r.kyle.norris@gmail.com>
2025-03-18 19:59:27 -0400

    began creating backgrounds models

--- models/character/backgrounds.py
+++ models/character/backgrounds.py
@@ -1,2 +1,28 @@
 from models.declarative_base import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+class Background(Base):
+    __tablename__ = 'backgrounds'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    description = Column(String)
+    features = relationship("BackgroundFeature", back_populates='background')
+    
+class BackgroundFeature(Base):
+    __tablename__ = 'background_features'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    background_id = Column(Integer, ForeignKey('backgrounds.id'))
+    name = Column(String)
+    description = Column(String)
+
+class BackgroundSkillBonus(Base):
+    __tablename__ = 'background_skill_bonuses'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    background_id = Column(Integer, ForeignKey('backgrounds.id'))
+    skill_id = Column(Integer, ForeignKey('skills.id'))
+    bonus = Column(Integer) # bonus to the skill
+    description = Column(String) # description of why the bonus is given
