3668f9219fa3420dbae173b3597c704f6de454bc
Kyle Norris <r.kyle.norris@gmail.com>
2025-03-19 19:34:55 -0400

    finished backgrounds classes

--- models/character/backgrounds.py
+++ models/character/backgrounds.py
@@ -26,3 +26,20 @@ class BackgroundSkillBonus(Base):
     skill_id = Column(Integer, ForeignKey('skills.id'))
     bonus = Column(Integer) # bonus to the skill
     description = Column(String) # description of why the bonus is given
+
+class BackgroundToolProficiency(Base):
+    __tablename__ = 'background_tool_proficiencies'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    background_id = Column(Integer, ForeignKey('backgrounds.id'))
+    tool_id = Column(Integer, ForeignKey('tools.id'))
+    description = Column(String) # description of why the proficiency is given
+
+class BackgroundStartingTools(Base):
+    __tablename__ = 'background_starting_tools'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    background_id = Column(Integer, ForeignKey('backgrounds.id'))
+    tool_id = Column(Integer, ForeignKey('tools.id'))
+    quantity = Column(Integer) # number of tools
+    description = Column(String) # description of why the tool is given
